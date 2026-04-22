f66c4977e687df4e374fc7eb5c0a31a0b0cc4185
Kajal Singh <ks9392656@gmail.com>
2026-04-22 20:52:51 +0530

    Updated the Main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-# This file majorly used for receiving the end points 
+from flask import Flask, render_template, flash, url_for, redirect
 
-from flask import Flask, render_template, url_for
+app = Flask(__name__)
 
-app = Flask(__name__, static_folder="assets")
+app.secret_key = "some secret message or key"
 
-# URL => End Point
 @app.route("/")
 def hello_world():
-    print(url_for("static", filename="style2.css"))
-    return render_template("index.html")
+    return redirect(url_for("login"))
 
-@app.route("/prime")
-def prime():
-    return "<p>Hello from Prime!</p>"
+@app.route("/login")
+def login():
+    return "<p>login page</p>"
+
+@app.route("/contact")
+def contact():
+    flash("support timings are from 9-5")
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
